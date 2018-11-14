b9ba557ff658ccc9679a7b74b91bc9a8f9a44c55
Rewaant Chhabra <developer.rewaant@gmail.com>
2018-11-14 15:21:51 +0530

    Using CSS animations in place of jQuery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-  $("#profile-photo, #icons, #links, span, h1, h2").hide().fadeIn(3000);
-  $("#border").animate({
-    borderRadius: "20%"
-  },1500)
-});
+// $(document).ready(function() {
+//   $("#profile-photo, #icons, #links, span, h1, h2").hide().fadeIn(3000);
+//   $("#border").animate({
+//     borderRadius: "20%"
+//   },1500)
+// });
