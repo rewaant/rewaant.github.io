2fd6250c1a30c286ae7785446c54d503a04085be
Rewaant Chhabra <developer.rewaant@gmail.com>
2018-11-20 15:01:31 +0530

    Minor content changes with added animations

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function() {
-//   $("#profile-photo, #icons, #links, span, h1, h2").hide().fadeIn(3000);
-//   $("#border").animate({
-//     borderRadius: "20%"
-//   },1500)
-// });
