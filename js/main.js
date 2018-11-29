63bea4324d886f83c467f907a3477887f4279f8d
Rewaant Chhabra <developer.rewaant@gmail.com>
2018-11-29 19:17:00 +0530

    Dropped AJAX and minor changes with animation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-$(function() {
+// $(function() {
 
-  var state = true;
+//   var state = true;
   
-  $("#sample-work-link").on("click", function(e){
-    e.preventDefault();
-    if(state == true) {
-      $("#body").load("portfolio.html");
-      $("#sample-work-link a").text("Back");
-      state = false;
-    }
-    else {
-      location.reload();
-    }
-  })
-});
+//   $("#sample-work-link").on("click", function(e){
+//     e.preventDefault();
+//     if(state == true) {
+//       $("#body").load("portfolio.html");
+//       $("#sample-work-link a").text("Back");
+//       state = false;
+//     }
+//     else {
+//       location.reload();
+//     }
+//   })
+// });
